27d1849baa3f23aa1f4dfab77a288a6ba765ce97
Ricardo Mogas <al066208@uacam.mx>
2023-12-18 00:33:04 -0600

    Implementación de electron y vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="mesage x"/>-->
+  <div class="sidebar" id="sidebar">
+    <CardMenu />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import CardMenu from "./components/CardMenu.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    CardMenu
   }
 }
 </script>
@@ -20,7 +24,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
 }
 </style>
